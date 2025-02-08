c78467176c6eed481e5562ecc32ef63491e7bc30
David Correa <escarriel@sudamericano.edu.ec>
2025-02-08 18:17:05 -0500

    registro con rol predeterminado

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,6 +10,8 @@ import * as bcrypt from 'bcrypt';
 import { JwtPayload } from './strategy/jwt-payload.interface';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { ValidRoles } from './interfaces/roles';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -21,53 +23,101 @@ export class AuthService {
   async login(loginDto: CreateLoginDto) {
     const { password, email } = loginDto;
 
-    const user = await this.prisma.users.findUnique({
+    const {
+      userId,
+      name,
+      email: userEmail,
+      password: userPassword,
+      roleId,
+    } = await this.prisma.users.findUnique({
       where: { email },
       select: {
+        name: true,
         email: true,
         password: true,
         userId: true,
+        roleId: true,
       },
     });
 
-    if (!user) throw new UnauthorizedException('Credentials are not valid');
+    if (!userId) throw new UnauthorizedException('Credentials are not valid');
 
-    if (!bcrypt.compareSync(password, user.password))
+    if (!bcrypt.compareSync(password, userPassword))
       throw new UnauthorizedException('Credentials are not valid');
 
+    const role = await this.prisma.roles.findUnique({
+      where: { roleId: roleId },
+      select: { name: true },
+    });
+
     const accessToken = this.getJwtToken(
-      { id: user.userId },
+      {
+        id: userId,
+        name: name,
+        email: userEmail,
+        role: role?.name,
+      },
       { expiresIn: '2d' },
     );
 
     return {
-      userId: user.userId,
+      userId: userId,
+      name: name,
+      email: userEmail,
+      role: role?.name || null,
       accessToken,
     };
   }
 
-  async register(createUserDto: CreateUserDto) {
+  async register(tx: Prisma.TransactionClient, createUserDto: CreateUserDto) {
     const hashedPassword = bcrypt.hashSync(createUserDto.password, 10);
 
+    let role;
+
+    if (createUserDto.roleId) {
+      role = await tx.roles.findUnique({
+        where: { roleId: createUserDto.roleId },
+      });
+
+      if (!role) {
+        throw new Error('Invalid roleId provided');
+      }
+    } else {
+      role = await tx.roles.findFirst({
+        where: { name: ValidRoles.user },
+      });
+
+      if (!role) {
+        throw new Error('Default role not found');
+      }
+    }
+
     try {
-      const user = await this.prisma.users.create({
+      const { userId, name, email } = await tx.users.create({
         data: {
           ...createUserDto,
           password: hashedPassword,
+          roleId: role.roleId,
         },
         select: {
           email: true,
           userId: true,
+          name: true,
         },
       });
 
       const accessToken = this.getJwtToken(
-        { id: user.userId },
+        {
+          id: userId,
+          name,
+          email,
+          role: role?.name,
+        },
         { expiresIn: '2d' },
       );
 
       return {
-        userId: user.userId,
+        userId: userId,
         accessToken,
       };
     } catch (error) {
